1dc9b20dd21d63c827b7c6ca7ed72c27ef12766f
raymondtju <raymond.rtju@gmail.com>
2023-03-25 11:28:27 +0700

    style: fix layout

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -8,9 +8,9 @@ function Layout({
   children: PropsWithChildren<React.ReactNode>;
 }) {
   return (
-    <div className="relative">
+    <div className="relative overflow-hidden">
       <div className="mx-auto max-w-[1440px]">
-        <div className="mx-[120px]">{children}</div>
+        <div className="mx-6 md:mx-8 lg:mx-[120px]">{children}</div>
       </div>
     </div>
   );
