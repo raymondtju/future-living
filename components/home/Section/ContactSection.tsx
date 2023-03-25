0e9d09bbc3a0969a78738adffeba7f63f05dc1a3
raymondtju <raymond.rtju@gmail.com>
2023-03-26 01:28:40 +0700

    contact us: mobile view

--- components/home/Section/ContactSection.tsx
+++ components/home/Section/ContactSection.tsx
@@ -3,45 +3,63 @@ import React from "react";
 
 import maps from "/public/assets/images/Maps.png";
 import Image from "next/image";
+import clsx from "clsx";
 
 function ContactSection() {
   return (
     <>
-      <h2 className="text-center text-14xl font-semibold text-Secondary-100">
+      <h2
+        className={clsx(
+          "text-center text-6xl font-semibold text-Secondary-100",
+          "md:text-[48px]"
+        )}
+      >
         Contact Us
       </h2>
       <div className="mt-[60px] flex flex-row flex-wrap gap-4">
         <div className="flex flex-grow flex-col gap-10">
           <div className="rounded-11xl border-[1px] border-Primary-80 px-8 py-6">
             <div className="flex flex-row text-Primary-80">
-              <Map className="m-2 w-8" />
+              <Map className="m-2 w-5 md:w-8" />
               <div className="flex flex-col pl-2">
-                <span className="text-7xl font-semibold">Location</span>
-                <span className="pt-2 text-2xl">Jln. Pahlawan no 16</span>
+                <span className={clsx("text-lg font-semibold", "md:text-7xl")}>
+                  Location
+                </span>
+                <span className={clsx("pt-2 text-2xs", "md:text-2xl")}>
+                  Jln. Pahlawan no 16
+                </span>
               </div>
             </div>
           </div>
           <div className="rounded-11xl border-[1px] border-Primary-80 px-8 py-6">
             <div className="flex flex-row text-Primary-80">
-              <Email className="m-2 w-8" />
+              <Email className="m-2 w-5 md:w-8" />
               <div className="flex flex-col pl-2">
-                <span className="text-7xl font-semibold">Email</span>
-                <span className="pt-2 text-2xl">service@gmail.com</span>
+                <span className={clsx("text-lg font-semibold", "md:text-7xl")}>
+                  Email
+                </span>
+                <span className={clsx("pt-2 text-2xs", "md:text-2xl")}>
+                  service@gmail.com
+                </span>
               </div>
             </div>
           </div>
           <div className="rounded-11xl border-[1px] border-Primary-80 px-8 py-6">
             <div className="flex flex-row text-Primary-80">
-              <Phone className="m-2 w-8" />
+              <Phone className="m-2 w-5 md:w-8" />
               <div className="flex flex-col pl-2">
-                <span className="text-7xl font-semibold">Call</span>
-                <span className="pt-2 text-2xl">+628678213018</span>
+                <span className={clsx("text-lg font-semibold", "md:text-7xl")}>
+                  Call
+                </span>
+                <span className={clsx("pt-2 text-2xs", "md:text-2xl")}>
+                  +628678213018
+                </span>
               </div>
             </div>
           </div>
         </div>
 
-        <Image src={maps} alt="maps" />
+        <Image src={maps} alt="maps" className="hidden md:flex" />
       </div>
     </>
   );
