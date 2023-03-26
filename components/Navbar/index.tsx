5cbfc4b3ed4304b626e0d1cb1218b20051e8b50a
raymondtju <raymond.rtju@gmail.com>
2023-03-26 17:04:01 +0700

    style: responsive navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
+import { Burger } from "../Icons";
 
 function Navbar() {
   return (
     <nav>
-      <div className="my-10 flex items-center justify-between">
-        <div className="flex items-center text-7xl font-bold uppercase text-Primary-100">
+      <div className="flex items-center justify-between my-4 md:my-10">
+        <div className="flex items-center text-lg font-bold uppercase text-Primary-100 md:text-7xl">
           future living
         </div>
-        <div className="flex items-center">
-          <ul className="flex gap-9 text-5xl text-Primary-100">
+        <div className="items-center hidden md:flex">
+          <ul className="flex text-5xl gap-9 text-Primary-100">
             <li>Home</li>
             <li>About</li>
             <li>Contact</li>
           </ul>
+        </div>
+        <div className="flex md:hidden">
+          <Burger className="w-8 text-Primary-100" />
         </div>
       </div>
     </nav>
